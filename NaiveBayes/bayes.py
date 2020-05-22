372adfa861d241b16c6333121bdd0051b6eb28e7
Primer-Fan <primer.ming@gmail.com>
2020-05-22 23:22:14 +0800

    朴素贝叶斯算法添加注释

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -9,20 +9,20 @@ def load_data_set():
 
 
 def bayes(data_set, features: list):
-    diseases = list(set([items[2] for items in data_set]))
+    diseases = list(set([items[2] for items in data_set]))  # 可能患病情况
     result, rate = None, 0.0
     PA = 1.0
-    for feature in features:
+    for feature in features:    # 计算贝叶斯公式分母, 在朴素贝叶斯中, 我们认为特征相互独立
         PA = PA * len([i for i in data_set if feature in i]) / len(data_set)
     if PA == 0.0:
         return None
     for disease in diseases:
-        PC = len([i for i in data_set if i[2] == disease]) / len(data_set)
+        PC = len([i for i in data_set if i[2] == disease]) / len(data_set)  # 计算P(C)
         PAC = 1.0
-        for feature in features:
+        for feature in features:    # 计算P(A|C), 由概率论知识, P(AB|C) = P(A|C)*P(B|C)
             PAC = PAC * len([i for i in data_set if feature in i and i[2] == disease]) / len([i for i in data_set if i[2] == disease])
-        P = PAC * PC / PA
-        if P > rate:
+        P = PAC * PC / PA   # 贝叶斯公式
+        if P > rate:    # 更新最优解
             result = disease
             rate = P
     return result, rate
