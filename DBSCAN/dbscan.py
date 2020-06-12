02b177baa7d7b39c5e3cfaf49d15e53e4704420f
Primer-Fan <primer.ming@gmail.com>
2020-06-12 21:03:44 +0800

    改进算法并添加注释

--- DBSCAN/dbscan.py
+++ DBSCAN/dbscan.py
@@ -1,28 +1,34 @@
 import numpy as np
 
 def load_data():
+
+    # points: 点坐标数据  labels: 点编号数据
     points = [[1, 3], [1, 2], [2, 4], [2, 3], [2, 2], [2, 1], [3, 2], [4, 2], [5, 3], [5, 2], [5, 1], [6, 2]]
     labels = [i for i in range(1, 13)]
     return points, labels
 
 
 def dbscan(r, minPoints, points, labels):
+
+    # 计算点间欧式距离
     distance = np.zeros([len(points), len(points)], dtype=float)
     for i in range(len(points)):
         for j in range(i + 1, len(points)):
             distance[i][j] = np.sqrt(np.square(points[i][0] - points[j][0]) + np.square(points[i][1] - points[j][1]))
             distance[j][i] = distance[i][j]
-    central_points = []
-    noise_points = []
-    edge_points = []
-    for i in range(len(points)):
+
+    central_points = []     # 中心点
+    noise_points = []       # 噪声点
+    edge_points = []        # 边缘点
+    for i in range(len(points)):    # 第一遍统计出中心点与噪声点
         num = len([t for t in distance[i] if t <= r])
         if num >= minPoints:
             central_points.append(i)
         else:
             noise_points.append(i)
     len_noise = len(noise_points)
-    for i in central_points:
+
+    for i in central_points:        # 将边缘点从噪声点列表中删除并加入边缘点列表
         j = 0
         while j < len_noise:
             if distance[i][noise_points[j]] <= r:
@@ -33,7 +39,7 @@ def dbscan(r, minPoints, points, labels):
             j += 1
 
     result = []
-    while len(central_points) > 0:
+    while len(central_points) > 0:      # 聚类
         tmp = set()
         remove_point = []
         for i in range(len(central_points)):
