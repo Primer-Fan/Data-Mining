818f898d484e0913fb67a143f8666950d2e6471a
Primer-Fan <primer.ming@gmail.com>
2020-06-13 19:47:17 +0800

    补全离散因子计算部分

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -29,7 +29,13 @@ def k_means(points, labels, k):
             central_points.append((sum(points[k][0] for k in cluster[i]) / len(cluster[i]),
                                    (sum(points[k][1] for k in cluster[i]) / len(cluster[i]))))
 
-    return [[labels[i] for i in item] for item in cluster]
+    of2 = [0 for i in range(k)]
+    for i in range(k):
+        for j in range(k):
+            of2[i] += len(cluster[j]) / len(points) * math.sqrt((central_points[i][0] - central_points[j][0]) ** 2
+                                                                + (central_points[i][1] - central_points[j][1]) ** 2)
+
+    return [[labels[i] for i in item] for item in cluster], of2
 
 
 def cal_dis(distance, centralpoints, points, k):
@@ -38,8 +44,9 @@ def cal_dis(distance, centralpoints, points, k):
             distance[i][j] = math.sqrt((points[i][0] - centralpoints[j][0]) ** 2 + (points[i][1] - centralpoints[j][1]) ** 2)
 
 
-
 if __name__ == "__main__":
     points, labels = load_data_set()
-    result = k_means(points, labels, 3)
-    print(result)
+    clusters, of2s = k_means(points, labels, 3)
+    for i in range(3):
+        print(clusters[i], '\tof2:', of2s[i])
+
